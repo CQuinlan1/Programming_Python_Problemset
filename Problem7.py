81ac121b9d90bdbe5c1db14e96081e9cd353d711
Celeste Quinlan <G00376296@gmit.ie>
2019-03-31 19:45:22 +0100

    Final version

--- Problem7.py
+++ Problem7.py
@@ -1,5 +1,5 @@
 #**************************
-# Created by Catherine Ann Celeste Quinlan
+# Created by Catherine Ann Celeste Quinlan.
 # This program will take a FLOATING POINT NUMBER as input and output its square root approximation.
 # QUESTION 7
 #*************************
