957b2febbe8e4b9683c130851b873ea7ed2fe663
Celeste Quinlan <G00376296@gmit.ie>
2019-03-31 19:56:19 +0100

    Final version

--- Problem10.py
+++ Problem10.py
@@ -1,5 +1,5 @@
 #*************************
-#Created by Catherine Ann Celeste Quinlan
+#Created by Catherine Ann Celeste Quinlan.
 # This program display a plot of the functions x x squared and 2 to the power of x in the range [0,4]
 # Problem10
 # ***********************
@@ -7,10 +7,10 @@
 
 from matplotlib import pyplot as plt
 # x-axis values 
-x = [0,1,2,3,4] 
+x = [0,1,2,3,4]   # Domain for x function
   
 # Y-axis values 
-y = [0,1,2,3,4] 
+y = [0,1,2,3,4]   # Range for x function
   
 # Function to plot 
 plt.plot(x,y) 
@@ -20,10 +20,10 @@ plt.show()
 
 
 # x-axis values 
-x = [0,1,2,3,4] 
+x = [0,1,2,3,4]  # Domain for x  squared
   
 # Y-axis values 
-y = [0,1,4,9,16] 
+y = [0,1,4,9,16]   # Range  for x  squared
   
 # Function to plot 
 plt.plot(x,y) 
@@ -32,10 +32,10 @@ plt.plot(x,y)
 plt.show() 
 
 # x-axis values 
-x = [0,1,2,3,4] 
+x = [0,1,2,3,4]  # Domain for 2 to the power of x 
   
 # Y-axis values 
-y = [1,2,8,16] 
+y = [1,2,8,16] # Range  for 2 to the power of x 
   
 # Function to plot 
 plt.plot(x,y) 
